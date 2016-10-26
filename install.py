8356277f66094b8ab663f49d4750ccde0cff69c7
Alex Morega <alex@grep.ro>
2016-10-26 15:38:41 +0200

    create virtualenvs

--- install.py
+++ install.py
@@ -1,14 +1,47 @@
+import sys
+import subprocess
 from pathlib import Path
+from contextlib import contextmanager
+from tempfile import TemporaryDirectory
+from urllib.request import urlretrieve
+
+VIRTUALENV_URL = 'https://github.com/pypa/virtualenv/raw/master/virtualenv.py'
+SETUPTOOLS_URL = 'https://pypi.python.org/packages/8a/1f/e2e14f0b98d0b6de6c3fb4e8a3b45d3b8907783937c497cb53539c0d2b19/setuptools-28.6.1-py2.py3-none-any.whl'
+PIP_URL = 'https://pypi.python.org/packages/9c/32/004ce0852e0a127f07f358b715015763273799bd798956fa930814b60f39/pip-8.1.2-py2.py3-none-any.whl'
 
 def question(label, default):
     rv = input("{} [{}]: ".format(label, default))
     return rv.strip() or default
+
+@contextmanager
+def tmp_virtualenv():
+    with TemporaryDirectory() as _tmp:
+        def download(url, directory):
+            name = url.split('/')[-1]
+            urlretrieve(url, str(directory / name))
+
+        def run(*args):
+            cmd = (
+                sys.executable,
+                tmp / 'virtualenv.py',
+                '--extra-search-dir={}'.format(tmp),
+            )
+            subprocess.check_call([str(c) for c in cmd + args])
+
+        tmp = Path(_tmp)
+        download(VIRTUALENV_URL, tmp)
+        download(SETUPTOOLS_URL, tmp)
+        download(PIP_URL, tmp)
+        yield run
 
 def main():
     home = Path(question("Installation folder", str(Path.cwd() / 'hoover')))
     if home.is_dir() and len(list(home.iterdir())) > 0:
         raise RuntimeError("Installation folder exists and is not empty")
     home.mkdir(exist_ok=True)
+    with tmp_virtualenv() as create_virtualenv:
+        create_virtualenv(home / 'venvs' / 'search')
+        create_virtualenv(home / 'venvs' / 'snoop')
 
 if __name__ == '__main__':
     main()
