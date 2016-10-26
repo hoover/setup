b74034e43060ffbfa2a790c217977aa5730f3f34
Alex Morega <alex@grep.ro>
2016-10-26 14:35:29 +0200

    create home folder

--- install.py
+++ install.py
@@ -1,10 +1,14 @@
+from pathlib import Path
+
 def question(label, default):
     rv = input("{} [{}]: ".format(label, default))
     return rv.strip() or default
 
 def main():
-    rv = question("Installation folder", '/opt/hoover')
-    print(repr(rv))
+    home = Path(question("Installation folder", str(Path.cwd() / 'hoover')))
+    if home.is_dir() and len(list(home.iterdir())) > 0:
+        raise RuntimeError("Installation folder exists and is not empty")
+    home.mkdir(exist_ok=True)
 
 if __name__ == '__main__':
     main()
