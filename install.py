17202a23343bf5b77af0ad8086eeedede7e42ea9
Alex Morega <alex@grep.ro>
2016-10-26 14:28:14 +0200

    ask where to install hoover

--- install.py
+++ install.py
@@ -1,1 +1,10 @@
-print("Hello world!")
+def question(label, default):
+    rv = input("{} [{}]: ".format(label, default))
+    return rv.strip() or default
+
+def main():
+    rv = question("Installation folder", '/opt/hoover')
+    print(repr(rv))
+
+if __name__ == '__main__':
+    main()
