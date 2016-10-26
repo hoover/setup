00a835cf725ece13c3e0b82bbbe435fa8183ff31
Alex Morega <alex@grep.ro>
2016-10-26 15:48:13 +0200

    clone repos

--- install.py
+++ install.py
@@ -8,10 +8,16 @@ from urllib.request import urlretrieve
 VIRTUALENV_URL = 'https://github.com/pypa/virtualenv/raw/master/virtualenv.py'
 SETUPTOOLS_URL = 'https://pypi.python.org/packages/8a/1f/e2e14f0b98d0b6de6c3fb4e8a3b45d3b8907783937c497cb53539c0d2b19/setuptools-28.6.1-py2.py3-none-any.whl'
 PIP_URL = 'https://pypi.python.org/packages/9c/32/004ce0852e0a127f07f358b715015763273799bd798956fa930814b60f39/pip-8.1.2-py2.py3-none-any.whl'
+SEARCH_REPO = 'https://github.com/hoover/search.git'
+SNOOP_REPO = 'https://github.com/hoover/snoop.git'
+UI_REPO = 'https://github.com/hoover/ui.git'
 
 def question(label, default):
     rv = input("{} [{}]: ".format(label, default))
     return rv.strip() or default
+
+def runcmd(cmd, **kwargs):
+    subprocess.check_call([str(c) for c in cmd], **kwargs)
 
 @contextmanager
 def tmp_virtualenv():
@@ -26,7 +32,7 @@ def tmp_virtualenv():
                 tmp / 'virtualenv.py',
                 '--extra-search-dir={}'.format(tmp),
             )
-            subprocess.check_call([str(c) for c in cmd + args])
+            runcmd(cmd + args)
 
         tmp = Path(_tmp)
         download(VIRTUALENV_URL, tmp)
@@ -34,14 +40,22 @@ def tmp_virtualenv():
         download(PIP_URL, tmp)
         yield run
 
+def clone_git(url, directory):
+    runcmd(['git', 'clone', url], cwd=str(directory))
+
 def main():
     home = Path(question("Installation folder", str(Path.cwd() / 'hoover')))
     if home.is_dir() and len(list(home.iterdir())) > 0:
         raise RuntimeError("Installation folder exists and is not empty")
     home.mkdir(exist_ok=True)
+
     with tmp_virtualenv() as create_virtualenv:
         create_virtualenv(home / 'venvs' / 'search')
         create_virtualenv(home / 'venvs' / 'snoop')
+
+    clone_git(SEARCH_REPO, home)
+    clone_git(SNOOP_REPO, home)
+    clone_git(UI_REPO, home)
 
 if __name__ == '__main__':
     main()
