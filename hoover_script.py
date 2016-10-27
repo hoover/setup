85538bb89a69f5b2bc207df07f4b7e719be602df
Alex Morega <alex@grep.ro>
2016-10-27 14:14:54 +0200

    move the bootstrapping code to `hoover_script.py`

--- hoover_script.py
+++ hoover_script.py
@@ -2,8 +2,26 @@ import sys
 import os
 import random
 import math
+import subprocess
 from pathlib import Path
+from tempfile import TemporaryDirectory
+from contextlib import contextmanager
 from textwrap import dedent
+from urllib.request import urlretrieve
+
+VIRTUALENV_URL = 'https://github.com/pypa/virtualenv/raw/master/virtualenv.py'
+SETUPTOOLS_URL = 'https://pypi.python.org/packages/8a/1f/e2e14f0b98d0b6de6c3fb4e8a3b45d3b8907783937c497cb53539c0d2b19/setuptools-28.6.1-py2.py3-none-any.whl'
+PIP_URL = 'https://pypi.python.org/packages/9c/32/004ce0852e0a127f07f358b715015763273799bd798956fa930814b60f39/pip-8.1.2-py2.py3-none-any.whl'
+SEARCH_REPO = 'https://github.com/hoover/search.git'
+SNOOP_REPO = 'https://github.com/hoover/snoop.git'
+UI_REPO = 'https://github.com/hoover/ui.git'
+
+HOOVER_SCRIPT = """\
+#!/bin/sh
+cd '{setup}'
+export HOOVER_HOME='{home}'
+{python} hoover_script.py "$@"
+"""
 
 _home = os.environ.get('HOOVER_HOME')
 if not _home:
@@ -13,6 +31,36 @@ home = Path(_home)
 def question(label, default):
     rv = input("{} [{}]: ".format(label, default))
     return rv.strip() or default
+
+def runcmd(cmd, **kwargs):
+    if 'env' not in kwargs:
+        kwargs['env'] = dict(os.environ)
+        kwargs['env'].pop('__PYVENV_LAUNCHER__', None)
+    subprocess.check_call([str(c) for c in cmd], **kwargs)
+
+@contextmanager
+def tmp_virtualenv():
+    with TemporaryDirectory() as _tmp:
+        def download(url, directory):
+            name = url.split('/')[-1]
+            urlretrieve(url, str(directory / name))
+
+        def run(*args):
+            cmd = (
+                sys.executable,
+                tmp / 'virtualenv.py',
+                '--extra-search-dir={}'.format(tmp),
+            )
+            runcmd(cmd + args)
+
+        tmp = Path(_tmp)
+        download(VIRTUALENV_URL, tmp)
+        download(SETUPTOOLS_URL, tmp)
+        download(PIP_URL, tmp)
+        yield run
+
+def git_clone(url, directory):
+    runcmd(['git', 'clone', url], cwd=str(directory))
 
 def random_secret_key(entropy=256):
     vocabulary = ('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
@@ -47,7 +95,6 @@ def configure_search():
         }}
         STATIC_ROOT = str(base_dir / 'static')
         HOOVER_UPLOADS_ROOT = str(base_dir / 'uploads')
-        HOOVER_ELASTICSEARCH_URL = {es_url!r}
         HOOVER_UI_ROOT = {ui_root!r}
     """)
     with local_py.open('w', encoding='utf-8') as f:
@@ -75,6 +122,7 @@ def configure_snoop():
             }}
         }}
         SNOOP_ROOT = {data_path!r}
+        SNOOP_ELASTICSEARCH_URL = {es_url!r}
     """)
     with local_py.open('w', encoding='utf-8') as f:
         f.write(template.format(**values))
@@ -83,6 +131,40 @@ def execv(args):
     os.execv(args[0], args)
 
 def main(argv):
+    if argv == ['bootstrap']:
+        git_clone(SEARCH_REPO, home)
+        git_clone(SNOOP_REPO, home)
+        git_clone(UI_REPO, home)
+
+        with tmp_virtualenv() as create_virtualenv:
+            create_virtualenv(home / 'venvs' / 'search')
+            create_virtualenv(home / 'venvs' / 'snoop')
+
+        venv = lambda name, cmd: home / 'venvs' / name / 'bin' / cmd
+        runcmd([
+            venv('search', 'pip'), 'install',
+            '-r', home / 'search' / 'requirements.txt',
+        ])
+        runcmd([
+            venv('snoop', 'pip'), 'install',
+            '-r', home / 'snoop' / 'requirements.txt',
+        ])
+        runcmd(['npm', 'install'], cwd=str(home / 'ui'))
+        runcmd(['./run', 'build'], cwd=str(home / 'ui'))
+
+        (home / 'bin').mkdir(exist_ok=True)
+        bin_hoover = home / 'bin' / 'hoover'
+        with bin_hoover.open('w', encoding='utf-8') as f:
+            f.write(HOOVER_SCRIPT.format(
+                python=sys.executable,
+                home=home,
+                setup=home / 'setup',
+            ))
+        bin_hoover.chmod(0o755)
+
+        print("Success!")
+        return
+
     if argv == ['configure']:
         configure_search()
         configure_snoop()
--- install.py
+++ install.py
@@ -1,59 +1,13 @@
 import sys
 import os
-import subprocess
 from pathlib import Path
-from contextlib import contextmanager
-from tempfile import TemporaryDirectory
-from urllib.request import urlretrieve
+import subprocess
 
-VIRTUALENV_URL = 'https://github.com/pypa/virtualenv/raw/master/virtualenv.py'
-SETUPTOOLS_URL = 'https://pypi.python.org/packages/8a/1f/e2e14f0b98d0b6de6c3fb4e8a3b45d3b8907783937c497cb53539c0d2b19/setuptools-28.6.1-py2.py3-none-any.whl'
-PIP_URL = 'https://pypi.python.org/packages/9c/32/004ce0852e0a127f07f358b715015763273799bd798956fa930814b60f39/pip-8.1.2-py2.py3-none-any.whl'
 SETUP_REPO = 'https://github.com/hoover/setup.git'
-SEARCH_REPO = 'https://github.com/hoover/search.git'
-SNOOP_REPO = 'https://github.com/hoover/snoop.git'
-UI_REPO = 'https://github.com/hoover/ui.git'
-
-HOOVER_SCRIPT = """\
-#!/bin/sh
-cd '{setup}'
-export HOOVER_HOME='{home}'
-{python} hoover_script.py "$@"
-"""
 
 def question(label, default):
     rv = input("{} [{}]: ".format(label, default))
     return rv.strip() or default
-
-def runcmd(cmd, **kwargs):
-    if 'env' not in kwargs:
-        kwargs['env'] = dict(os.environ)
-        kwargs['env'].pop('__PYVENV_LAUNCHER__', None)
-    subprocess.check_call([str(c) for c in cmd], **kwargs)
-
-@contextmanager
-def tmp_virtualenv():
-    with TemporaryDirectory() as _tmp:
-        def download(url, directory):
-            name = url.split('/')[-1]
-            urlretrieve(url, str(directory / name))
-
-        def run(*args):
-            cmd = (
-                sys.executable,
-                tmp / 'virtualenv.py',
-                '--extra-search-dir={}'.format(tmp),
-            )
-            runcmd(cmd + args)
-
-        tmp = Path(_tmp)
-        download(VIRTUALENV_URL, tmp)
-        download(SETUPTOOLS_URL, tmp)
-        download(PIP_URL, tmp)
-        yield run
-
-def git_clone(url, directory):
-    runcmd(['git', 'clone', url], cwd=str(directory))
 
 def main():
     home = Path(question("Installation folder", str(Path.cwd() / 'hoover')))
@@ -61,38 +15,13 @@ def main():
         raise RuntimeError("Installation folder exists and is not empty")
     home.mkdir(exist_ok=True)
 
-    with tmp_virtualenv() as create_virtualenv:
-        create_virtualenv(home / 'venvs' / 'search')
-        create_virtualenv(home / 'venvs' / 'snoop')
-
-    git_clone(SETUP_REPO, home)
-    git_clone(SEARCH_REPO, home)
-    git_clone(SNOOP_REPO, home)
-    git_clone(UI_REPO, home)
-
-    venv = lambda name, cmd: home / 'venvs' / name / 'bin' / cmd
-    runcmd([
-        venv('search', 'pip'), 'install',
-        '-r', home / 'search' / 'requirements.txt',
-    ])
-    runcmd([
-        venv('snoop', 'pip'), 'install',
-        '-r', home / 'snoop' / 'requirements.txt',
-    ])
-    runcmd(['npm', 'install'], cwd=str(home / 'ui'))
-    runcmd(['./run', 'build'], cwd=str(home / 'ui'))
-
-    (home / 'bin').mkdir(exist_ok=True)
-    bin_hoover = home / 'bin' / 'hoover'
-    with bin_hoover.open('w', encoding='utf-8') as f:
-        f.write(HOOVER_SCRIPT.format(
-            python=sys.executable,
-            home=home,
-            setup=home / 'setup',
-        ))
-    bin_hoover.chmod(0o755)
-
-    print("Success! Next step: run `{} configure`".format(bin_hoover))
+    subprocess.check_call(['git', 'clone', SETUP_REPO], cwd=str(home))
+    args = [
+        sys.executable,
+        str(home / 'setup' / 'hoover_script.py'),
+        'bootstrap',
+    ]
+    os.execve(args[0], args, dict(os.environ, HOOVER_HOME=str(home)))
 
 if __name__ == '__main__':
     main()
