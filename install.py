19f42604aa42300ff22474a0aef0c119265322ef
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2017-04-06 12:43:45 +0300

    make the installer read params from envvars
    The install.py script will now read from the HOOVER_HOME,
    HOOVER_SETUP_REPO, HOOVER_SETUP_BRANCH environment vars.

--- install.py
+++ install.py
@@ -3,19 +3,22 @@ import os
 from pathlib import Path
 import subprocess
 
-SETUP_REPO = 'https://github.com/hoover/setup.git'
+SETUP_REPO = os.getenv('HOOVER_SETUP_REPO', 'https://github.com/hoover/setup.git')
+SETUP_BRANCH = os.getenv('HOOVER_SETUP_BRANCH', 'master')
 
-def question(label, default):
-    rv = input("{} [{}]: ".format(label, default))
-    return rv.strip() or default
+if os.getenv('HOOVER_HOME'):
+    home = Path(os.getenv('HOOVER_HOME'))
+else:
+    home = Path.cwd() / 'hoover'
 
 def main():
-    home = Path(question("Installation folder", str(Path.cwd() / 'hoover')))
     if home.is_dir() and len(list(home.iterdir())) > 0:
-        raise RuntimeError("Installation folder exists and is not empty")
+        print("Installation folder {} exists and is not empty.".format(str(home)))
+        print("Please specify a suitable path using the HOOVER_HOME environment variable.")
     home.mkdir(exist_ok=True)
 
     subprocess.check_call(['git', 'clone', SETUP_REPO], cwd=str(home))
+    subprocess.check_call(['git', 'checkout', SETUP_BRANCH], cwd=str(home / 'setup'))
     args = [
         sys.executable,
         str(home / 'setup' / 'hoover_script.py'),
